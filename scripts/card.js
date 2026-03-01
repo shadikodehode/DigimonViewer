2b3bfa2ec205882048e623b27f7179c3fca801d5
shadikodehode <shadi.kodehode@gmail.com>
2026-03-01 20:34:13 +0100

    Testing git in vsc

--- scripts/card.js
+++ scripts/card.js
@@ -2,13 +2,34 @@ const cards = [{
   image: 'cards/Baboongamon.png',
   name: 'Baboongamon',
   level: 'ultimate'
+}, {
+  image: 'cards/Agumon.png',
+  name: 'Agumon',
+  level: 'rookie'
+}, {
+  image: 'cards/Dracomon (X-Antibody).png',
+  name: 'Dracomon',
+  level: 'champion'
+}, {
+  image: 'cards/Shoeshoemon.png',
+  name: 'Shoeshoemon',
+  level: 'training1'
+}, {
+  image: 'cards/Sistermon Ciel.png',
+  name: 'Sistermon',
+  level: 'training2'
+}, {
+  image: 'cards/Starmon.png',
+  name: 'Starmon',
+  level: 'mega'
 }];
 
 let cardsHTML = '';
 
 cards.forEach((card) => {
   cardsHTML += `
-    <div class="card">
+    <div class="card
+    js-card">
 			<div class="card-appearance">
 				<img class="card-image" 
         src="${card.image}">
@@ -27,3 +48,10 @@ cards.forEach((card) => {
 
 document.querySelector('.js-card-container').
 innerHTML = cardsHTML;
+
+document.querySelectorAll('.js-card')
+  .forEach((card)=>{
+      card.addEventListener('click', () => {
+        console.log('clicked card')
+      });
+  });

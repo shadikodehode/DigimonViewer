f364be69d0c1e502d25960b12da2abdb67e0ae83
shadikodehode <shadi.kodehode@gmail.com>
2026-03-06 13:04:43 +0100

    DigimonViewer
    Refractoring everything

--- data/digimonAPI.js
+++ data/digimonAPI.js
@@ -1,10 +1,88 @@
 
-document.addEventListener('scroll', (event) => {
-  if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000){
-    fetchData();
-  }
-});
+const digimonApi = 'https://digi-api.com/api/v1';
 
+let digimonCollection = [];
+
+const cardContainer = document.querySelector('.card-container')
+
+const createCard = () => {
+  const cardElement = document.createElement('div');
+    cardElement.className = "card";
+  
+    cardContainer.appendChild(cardElement);
+
+  const cardAppearance = document.createElement('div');
+    cardAppearance.className = "card-appearance";
+
+    cardElement.appendChild(cardAppearance);
+}
+createCard();
+
+// const fetchDigimon = async () => {
+//   const response = await fetch(`${digimonApi}/digimon`)
+//   return await response.json();
+// }
+
+
+// const fetchDigimonByUrl = async (href) => {
+//   const response = await fetch(href);
+
+//   return await response.json();
+// }
+
+// const fetchDigimonData = async (data) => {
+//   for (const digimonList of data.content) {
+//     const digimon = await fetchDigimonByUrl(digimonList.href);
+//     digimonCollection.push({ ...digimon});
+//   }
+// }
+
+// //turn this into a card later
+// const addButton = () => {
+//   const buttonEl = document.createElement('button');
+//   buttonEl.textContent = "Fetch Digimon api test";
+
+//   buttonEl.addEventListener('click', async () => {
+//     digimonCollection = [];
+//     const data = await fetchDigimon();
+//     await fetchDigimonData(data);
+//     console.log(data)
+//     renderCard();
+//   })
+//   return buttonEl
+// }
+
+// const displayDigimon = (digimonList) => {
+//   const imageEl = document.createElement('img');
+//     imageEl.src = digimonList.images[0].href;
+
+//   imageEl.height = 150;
+  
+//   return imageEl
+// }
+
+// const renderCard = () => {
+//   cardContainer.replaceChildren();
+//   cardContainer.append(addButton());
+//   digimonCollection.forEach(digimon => {
+//       const cardDiv = document.createElement('div');
+//       cardDiv.append(
+//       displayDigimon(digimon)
+//       )
+//       cardContainer.append(cardDiv)
+//     })
+// }
+
+// renderCard();
+
+//load more on scroll function//
+// fetchData();
+
+// document.addEventListener('scroll', (event) => {
+//   if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000){
+//     fetchData();
+//   }
+// });
 
 const cards = [{
   id: '1f1',
--- scripts/card.js
+++ scripts/card.js
@@ -1,138 +1,146 @@
-class Digimon {
-  id;
-  name;
-  digimon;
-  image;
-  level;
-  attribute;
-  field;
-  skills;
-  release;
-  type;
+// let cardHTML = '';
 
-  constructor(digimonDetails) {
-    this.id = digimonDetails.id;
-    this.name = digimonDetails.name;
-    this.image = digimonDetails.image;
-    this.level = digimonDetails.level;
-    this.attribute = digimonDetails.attribute;
-    this.field = digimonDetails.field;
-    this.skill = digimonDetails.skill;
-    this.release = digimonDetails.release;
-    this.type = digimonDetails.type;
-  }
-  display(){
-      return `${this.id, this.name}`
-  }
-}
+// let modalHTML = '';
 
-async function getDigimon() {
-  try{
-    const response = await fetch("https://digi-api.com/api/v1/digimon")
-    const data = await response.json();
+// class Digimon {
+//   id;
+//   name;
+//   digimon;
+//   image;
+//   level;
+//   attribute;
+//   field;
+//   skills;
+//   release;
+//   type;
 
-    for(let i = 0; i < data.length; i++){
-      console.log(i)
-    }
-  }  catch (error){
-    console.error('Error fetching data', error);
-  }
-};
-getDigimon();
-// async function fetchData() {
-//   try{
-//     const response = await fetch("https://digi-api.com/api/v1/digimon")
-//     if(!response.ok){
-//       throw new Error("Could not fetch")
-//     }
-    
-//     const data = await response.json()
-//     data.content.map((digimonDetails) => {
-//       return new Digimon(digimonDetails);
-//     });
-//     console.log(data);
-//     data;
-
+//   constructor(digimonDetails) {
+//     this.id = digimonDetails.id;
+//     this.name = digimonDetails.name;
+//     this.image = digimonDetails.image;
+//     this.level = digimonDetails.level;
+//     this.attribute = digimonDetails.attribute;
+//     this.field = digimonDetails.field;
+//     this.skill = digimonDetails.skill;
+//     this.release = digimonDetails.release;
+//     this.type = digimonDetails.type;
 //   }
-//   catch(error){
-//     console.error(error);
-//   }};
-//   fetchData();
+// }
+//   //params: id,  name, leave blank  to get list of
 
-// async function getDigimonId(){
-//   Digimon.forEach((id, index) => {
-//     console.log(`Index: ${index}, id: ${id}`)
+//   getIdUrl() {
+//     return `https://digi-api.com/api/v1/digimon/${this.id}`;
+//   }
+
+//   getNameUrl() {
+//     return `https://digi-api.com/api/v1/digimon/${this.name}`;
+//   }
+
+//   getDigimonUrl() {
+//     return `https://digi-api.com/api/v1/digimon/${this.digimon}`; //params: name, exact, attribute, level, page, pageSize, leaveblank to get list of digimons
+//   }
+
+//   getImageUrl() {
+//     return `https://digi-api.com/api/v1/digimon/${this.image}`;
+//   }
+
+//   getLevelUrl() {
+//     return `https://digi-api.com/api/v1/level/${this.level}`;
+//   }
+
+//   getAttributeUrl() {
+//     return `https://digi-api.com/api/v1/attribute/${this.attribute}`;
+//   }
+
+//   getFieldUrl() {
+//     return `https://digi-api.com/api/v1/field/${this.field}`;
+//   }
+
+//   getSkillUrl() {
+//     return `https://digi-api.com/api/v1/skill/${this.skill}`;
+//   }
+
+//   getReleaseUrl() {
+//     return `https://digi-api.com/api/v1/digimon/${this.release}`;
+//   }
+
+//   getTypeUrl() {
+//     return `https://digi-api.com/api/v1/type/${this.type}`;
+//   }
+// }
+
+
+// generateCard();
+// function generateCard(){
+//     cards.forEach((card) => { 
+//       cardHTML += `
+//         <div class="card 
+//         js-card
+//         "
+//         data-card-id="${card.id}">
+//           <div class="card-appearance">
+
+//             <img class="card-image" 
+//             src="${card.image}">
+
+//             <div class="card-name">
+//             ${card.name}
+//             </div>
+
+//             <div class="card-level-${card.level}">
+//             ${card.level}
+
+//             </div>
+//           </div> 
+//         </div>
+//       `;
+// })
+// };
+
+// digimonClicked()
+//   function digimonClicked() {
+//   document.querySelectorAll('.js-card')
+//     .forEach((card) => {
+//         card.addEventListener('click', () => {
+//           console.log(card.dataset.cardId);
+//         })
+//     });
+//   };
+
+// function modalCard(){
+//     modal.onClick(() => {
+//         modal.style.display = "flex",
+//           this.innerHTML += `
+//             <div class="modal-content">
+//               test
+//             </div>
+//           `;
+//     },
+//       window.onclick = function(event) {
+//         if (event.target == modal) {
+//             modal.style.display = "none";
+//         }
+//       })
+//   }
+
+// modalsHTML();
+
+// function modalsHTML(){
+//   modalPopup.onclick(() => {
+//     modalHTML += `
+//       <div class="modal-content">
+//         test
+//       </div>
+//     `;
 //   });
 // }
-// getDigimonId();
 
-// function getDigimonId(){
-//   for(let digimon of data){
-//     console.log(digimon)
-//   }
-// }
-// getDigimonId();
+// document.querySelector('.js-card-container')
+//   .innerHTML = cardHTML;
 
+// document.querySelector('.js-modal-container')
+//   .innerHTML = modalHTML;
 
-let cardHTML = '';
+// let modal = document.querySelector('.js-modal-container')
 
-let modalHTML = '';
-
-generateCard();
-
-function generateCard(){
-    cards.forEach((card) => { 
-      cardHTML += `
-        <div class="card 
-        js-card
-        "
-        data-card-id="${card.id}">
-          <div class="card-appearance">
-
-            <img class="card-image" 
-            src="${card.image}">
-
-            <div class="card-name">
-            ${card.name}
-            </div>
-
-            <div class="card-level-${card.level}">
-            ${card.level}
-
-            </div>
-          </div> 
-        </div>
-      `;
-});
-}
-
-document.querySelector('.js-card-container')
-  .innerHTML = cardHTML;
-
-  document.querySelector('.js-modal-container')
-    .innerHTML = modalHTML;
-  
-const modalClicked = document.getElementById('.js-card');
-const modal = document.getElementById('.js-modal')
-
-function digimonClicked() {
-  document.querySelectorAll('.js-card')
-  .forEach((card) => {
-      card.addEventListener('click', () => {
-        modalClicked.addEventListener('click', () => {
-          modal.style.display = "flex";
-        })
-        modalHTML += `
-          <div id="js-modal">
-            <div>
-            test
-            </div>
-          </div>
-        `;
-        console.log(card.dataset.cardId);
-      })
-  })
-}
-digimonClicked()
-
-
+// let cardPopper = document.querySelector('.js-modal-content')

7217eea904600adf1b967090d39e31941bd22fa0
shadikodehode <shadi.kodehode@gmail.com>
2026-03-07 01:08:16 +0100

    DigimonViewer
    Fixed infinite load
    
    the api isn't set up with a pageSize so I had some issues making it  work as intended(my fault for not reading the api properly)

--- data/digimonAPI.js
+++ data/digimonAPI.js
@@ -4,8 +4,16 @@ const digimonApi = 'https://digi-api.com/api/v1';
 
 let digimonArr = [];
 
+let isLoading = false;
+
+let currentPage = 0;
+
+let totalPages = 0;
+
+const pageSize = 40;
+
 const fetchDigimon = async () => {
-  const response = await fetch(`${digimonApi}/digimon?pageSize=20`)
+  const response = await fetch(`${digimonApi}/digimon?pageSize=${pageSize}`)
   return await response.json();
 }
 
@@ -15,11 +23,10 @@ const fetchDigimonData = async (data) => {
     id: digimonList.id,
     name: digimonList.name,
     image: digimonList.image
-   });
-   console.log(digimonList)
-   console.log(data)
+   })
   }
-  console.log(digimonArr)
+  totalPages = data.pageable?.totalPages;
+  console.log(data)
 }
 
 export const getDigimon = async () => {
@@ -30,50 +37,25 @@ export const getDigimon = async () => {
   })
 }
 
-// //turn this into a card later
-// const addButton = () => {
-//   const buttonEl = document.createElement('button');
-//   buttonEl.textContent = "Fetch Digimon api test";
+const LoadNextPage = async () => {  
+  if(isLoading || currentPage >= totalPages) return;
+  isLoading = true;
+  currentPage++;
+  const  response = await fetch(`${digimonApi}/digimon?pageSize=${pageSize}&page=${currentPage}`);
+  const data = await response.json();
+  const startLoad = digimonArr.length;
+  await fetchDigimonData(data);
+  digimonArr.slice(startLoad).forEach(digimon => {
+    createCard(digimon.name, digimon.image)
+  })
+  isLoading = false;
+    }
 
-//   buttonEl.addEventListener('click', async () => {
-//     digimonCollection = [];
-//     const data = await fetchDigimon();
-//     await fetchDigimonData(data);
-//     console.log(data)
-//     renderCard();
-//   })
-//   return buttonEl
-// }
+ document.addEventListener('scroll', () => {
+  if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 200){
+    LoadNextPage();
+  }}
+)
 
-// const displayDigimon = (digimonList) => {
-//   const imageEl = document.createElement('img');
-//     imageEl.src = digimonList.images[0].href;
 
-//   imageEl.height = 150;
-  
-//   return imageEl
-// }
-
-// const renderCard = () => {
-//   cardContainer.replaceChildren();
-//   cardContainer.append(addButton());
-//   digimonCollection.forEach(digimon => {
-//       const cardDiv = document.createElement('div');
-//       cardDiv.append(
-//       displayDigimon(digimon)
-//       )
-//       cardContainer.append(cardDiv)
-//     })
-// }
-
-// renderCard();
-
-//load more on scroll function//
-// fetchData();
-
-// document.addEventListener('scroll', (event) => {
-//   if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000){
-//     fetchData();
-//   }
-// });
 

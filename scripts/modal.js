42c1a5a59fb3346dc6c7ae07f66ce03cfb4cc3bd
shadikodehode <shadi.kodehode@gmail.com>
2026-03-11 14:04:03 +0100

    DigimonViewer
    Card appearance WIP
    
    planning on adding more functions to the cards, like clicking o the type to get a filter list of that types
    clicking on skills to get a list of the skills etc

--- scripts/modal.js
+++ scripts/modal.js
@@ -21,49 +21,58 @@ export const modal = async (id) => {
     modalName.className = 'modal-name'
 
     modalCard.appendChild(modalName)
+
+    const exitButton = document.createElement('div')
+  
+    exitButton.addEventListener('click', () => {
+      document.body.removeChild(darkenBackground)
+    })
+    exitButton.textContent = 'x'
+    exitButton.className = 'modal-exit-btn'
+    
+    modalCard.appendChild(exitButton)
   
   const modalLevel = document.createElement('div')
-    modalLevel.textContent = data.levels[0].level
+    modalLevel.textContent = data.levels[0]?.level
     modalLevel.className = 'modal-level'
 
     modalCard.appendChild(modalLevel)
 
   const modalType = document.createElement('div')
-    modalType.textContent = data.types[0].type
+    modalType.textContent =  data.types[0]?.type 
     modalType.className = 'modal-type' 
 
     modalCard.appendChild(modalType)
 
   const modalAttribute = document.createElement('div')
-    modalAttribute.textContent = data.attributes[0].attribute
+    modalAttribute.textContent = data.attributes[0]?.attribute
     modalAttribute.className = 'modal-attribute'
 
     modalCard.appendChild(modalAttribute)
+
+  const modalImage = document.createElement('img')
+    modalImage.src = data.images[0]?.href ?? 'Unknown'
+
+    modalCard.appendChild(modalImage)
   
   const modalDescription = document.createElement('div')
     modalDescription.textContent = data.descriptions
     .find(d => d.language === 'en_us')?.description
+    modalAttribute.className = 'modal-description'
 
     modalCard.appendChild(modalDescription)
 
-  const modalImage = document.createElement('img')
-    modalImage.src = data.images[0]?.href
-
-    modalCard.appendChild(modalImage)
-
-  const exitButton = document.createElement('div')
-  
-  exitButton.addEventListener('click', () => {
-    document.body.removeChild(darkenBackground)
-  })
-  exitButton.textContent = 'x'
-  exitButton.className = 'modal-exit-btn'
-  
-  modalCard.appendChild(exitButton)
   
   darkenBackground.addEventListener('click', (event) => {
   if(!modalCard.contains(event.target)) {
     document.body.removeChild(darkenBackground)}
-})
-}
+  })
 
+  const exitKey = (event) => {
+    if(event.key === 'Escape') {
+      document.body.removeChild(darkenBackground)
+      document.removeEventListener('keydown', exitKey)
+    }
+  }
+  document.addEventListener('keydown', exitKey)
+}
